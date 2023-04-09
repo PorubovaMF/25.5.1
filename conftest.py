7654b35cff83960198bb45dd203e48fc07a71adb
Marina Porubova <118303384+PorubovaMF@users.noreply.github.com>
2023-04-10 01:06:05 +0300

    Update

--- conftest.py
+++ conftest.py
@@ -33,4 +33,4 @@ def go_to_my_pets():
 
    element = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.LINK_TEXT, "Мои питомцы")))
    # Нажимаем на ссылку "Мои питомцы"
-   ytest.driver.find_element(By.LINK_TEXT, "Мои питомцы").click()
+   pytest.driver.find_element(By.LINK_TEXT, "Мои питомцы").click()
